d329be544e4674600275f852a1304d4688c921de
Jesus Campo Alvarez <eduardocampo91@gmail.com>
2024-07-09 22:20:49 -0500

    loading component reactrive implementation finished

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -41,18 +41,21 @@ export class HomeComponent implements OnInit {
 
   reloadCourses () {
 
-    this.loadingService.loadingOn();
+    //this.loadingService.loadingOn();
     // we are not calling the backend directly with this diferent approach
     const courses$ = this.coursesService.loadAllCourses()
     .pipe(
-      map(courses => courses.sort(sortCoursesBySeqNo)),
-      finalize(() => this.loadingService.loadingOff())
+      map(courses => courses.sort(sortCoursesBySeqNo))
+      //finalize(() => this.loadingService.loadingOff())
     );
-    this.beginnerCourses$ = courses$
+
+    const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$);
+
+    this.beginnerCourses$ = loadCourses$ 
     .pipe(
       map(courses => courses.filter(course => course.category == "BEGINNER"))
     );
-    this.advancedCourses$ = courses$
+    this.advancedCourses$ = loadCourses$ 
     .pipe(
       map(courses => courses.filter(course => course.category == "ADVANCED"))
     );
