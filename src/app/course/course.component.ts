c301ad7cd6ce6586b8e8a84cd5b1d4fd74e736fa
Jesus Campo Alvarez <eduardocampo91@gmail.com>
2024-08-06 23:15:32 -0500

    update display course and lessons component with single data pattern

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -13,10 +13,15 @@ import {
   withLatestFrom,
   concatAll, shareReplay, catchError
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, throwError} from 'rxjs';
+import {merge, fromEvent, Observable, concat, throwError, combineLatest} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { CoursesService } from '../services/courses.services';
 
+//interface to create a single data option and avoid nested ng containers and posible failures for mising data
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 
 @Component({
   selector: 'course',
@@ -25,9 +30,11 @@ import { CoursesService } from '../services/courses.services';
 })
 export class CourseComponent implements OnInit {
 
-  course$: Observable<Course>;
+  // course$: Observable<Course>;
 
-  lessons$: Observable<Lesson[]>;
+  // lessons$: Observable<Lesson[]>;
+
+  data$: Observable<CourseData>;
 
   constructor(private route: ActivatedRoute,
               private courseService: CoursesService
@@ -35,9 +42,25 @@ export class CourseComponent implements OnInit {
 
   ngOnInit() {
     const courseId = parseInt(this.route.snapshot.paramMap.get("courseId"));
-    this.course$ = this.courseService.loadCourseById(courseId);
-    this.lessons$ = this.courseService.loadAllLessons(courseId);
+    const course$ = this.courseService.loadCourseById(courseId)
+      .pipe(
+        startWith(null)
+      );
+    const lessons$ = this.courseService.loadAllLessons(courseId)
+      .pipe(
+        startWith([])
+      );
 
+    this.data$ = combineLatest([course$, lessons$])
+      .pipe(
+        map(([course, lessons]) => {
+          return {
+            course,
+            lessons
+          }
+        }),
+        tap(console.log)
+      );
   }
 
 
